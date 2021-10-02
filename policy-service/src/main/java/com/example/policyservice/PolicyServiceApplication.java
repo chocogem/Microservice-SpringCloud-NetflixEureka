4043e024d4726fb0f06dafca61a65190c4f5cb1d
Prapassorn Ukakimapun (Thailand) <uprapassorn@gmail.com>
2021-10-03 06:53:09 +0700

    Modified application class

--- policy-service/src/main/java/com/example/policyservice/PolicyServiceApplication.java
+++ policy-service/src/main/java/com/example/policyservice/PolicyServiceApplication.java
@@ -1,15 +1,17 @@
-package com.example.claimservice;
+package com.example.policyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ClaimServiceApplication {
+public class PolicyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClaimServiceApplication.class, args);
+		SpringApplication.run(PolicyServiceApplication.class, args);
 	}
 
 }
