0813169fd8ac0f88f8954343e48cf76c8851a2c8
Prapassorn Ukakimapun (Thailand) <uprapassorn@gmail.com>
2021-10-03 06:49:45 +0700

    First initial project structure

---
+++ insurance-policy-service/src/main/java/com/example/insurancepolicyservice/InsurancePolicyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancepolicyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePolicyServiceApplication.class, args);
+	}
+
+}
