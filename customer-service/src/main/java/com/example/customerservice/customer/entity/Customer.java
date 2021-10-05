6e60151f02505d514f0f074d72906a14e354f919
Prapassorn Ukakimapun (Thailand) <uprapassorn@gmail.com>
2021-10-06 04:03:15 +0700

    modified entity

--- customer-service/src/main/java/com/example/customerservice/customer/entity/Customer.java
+++ customer-service/src/main/java/com/example/customerservice/customer/entity/Customer.java
@@ -1,6 +1,7 @@
 package com.example.customerservice.customer.entity;
 
 import com.example.customerservice.policy.entity.Policy;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @Document
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Customer {
     @Id
     private String customerId;
--- policy-service/src/main/java/com/example/policyservice/claim/entity/Claim.java
+++ policy-service/src/main/java/com/example/policyservice/claim/entity/Claim.java
@@ -1,20 +1,20 @@
 package com.example.policyservice.claim.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-
 @Data
 @Document
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Claim {
     @Id
     private String claimNo;
     private String policyNo;
-    private Date admitDate;
-    private Date dischargeDate;
+    private String admitDate;
+    private String dischargeDate;
     private String claimStatus;
     private String hospitalCode;
 }
